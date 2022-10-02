b588a206cfdf53b843935989d28f82fdd7375e26
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-02 20:28:56 +0000

    Adding tarfile member sanitization to extractall()

--- space/wspace.py
+++ space/wspace.py
@@ -192,7 +192,26 @@ class Workspace:
         directory = self.get_unique_name() if rename else self.WORKSPACES
 
         with tarfile.open(filepath, "r:gz") as tar:
-            tar.extractall(directory)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(tar, directory)
 
         if rename:
             directory.joinpath(self.name).replace(self.WORKSPACES.joinpath(rename))
